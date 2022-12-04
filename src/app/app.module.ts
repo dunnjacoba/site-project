f0827b22d10ae28fc53c5cbc8d410b1708657a79
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-12-03 20:56:56 -0600

    banner format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { BannerComponent } from './banner/banner.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    BannerComponent
+    BannerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
